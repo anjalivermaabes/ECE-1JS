59a8d78943b7d9b348e4c66100b61dd4e37cf8fc
anjali verma <av3090075@gamil.com>
2026-02-25 10:16:15 +0530

    key value pair

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
-/*console.log("Hello, World!");*/
+/*console.log("Hello, World!");
 var num =10;
 console.log(num);
 console.log(typeof num);
-var num =;
+var num = "10";
 console.log(num);
 console.log(typeof num);
+var num = null;
+console.log(num);
+console.log(typeof num);
+var num = undefined;
+console.log(num);
+console.log(typeof num);*/
+var person = {
+    name: "John",
+    age: 30,
+    city: "New York"
+};
+console.log(person);
+console.log(person.name);
+console.log(person.age);
+console.log(person.city);
+const sym1 = Symbol(4)
+console.log(sym1);
+const sym2 = Symbol(4)
+console.log(sym2);
+if(sym1 === sym2){
+    console.log("true");
+}
+else{
+    console.log("false");
+}
