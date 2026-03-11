9e8fa86e7d1d48b83cb7647d1de1e3e120b84aba
anjali verma <av3090075@gamil.com>
2026-03-11 09:45:33 +0530

    Conditional Statement

--- script1.js
+++ script1.js
@@ -1,21 +1,53 @@
 //let i;
-for(let i = 0; i < 5; i++){
-    document.write(i + "<br>");
-}
-let i = 0;
-for(; i < 5; i++){
-    document.write(i + "<br>");
-}
-document.write("Loop stopped");
-for(key in object){
+//for(let i = 0; i < 5; i++){
+    //document.write(i + "<br>");
+//}
+//let i = 0;
+//for(; i < 5; i++){
+    //document.write(i + "<br>");
+//}
+//document.write("Loop stopped");
+//for(key in object){
  //code block to be executed
+//}
+//const person = {
+  //  fname: "Anita",
+    //lname: "Sharma",
+    //age: 30,
+//};
+//for(let x in person){
+  // console.log(x + ": " + person[x]);
+//}
+//coditional statements
+//if(condition){
+    //code block to be executed if condition is true}
+    //else if(condition){
+        //code block to be executed if condition is true}
+        //else{
+            //code block to be executed if condition is false}
+let age = 15;
+if(age > 18){
+    console.log("You are an adult");}
+    else{
+        console.log("You are a minor");
+    }
+    let grade = 0;
+switch(grade)
+{
+    case 90:
+        console.log("A");
+        break;
+    case 80:
+        console.log("B");
+        break;
+    case 70:
+        console.log("C");
+        break;
+    case 60:
+        console.log("D");
+        break;
+    default:
+        console.log("F");
 }
-const person = {
-    fname: "Anita",
-    lname: "Sharma",
-    age: 30,
-};
-for(let x in person){
-   console.log(x + ": " + person[x]);
-}
+
 
