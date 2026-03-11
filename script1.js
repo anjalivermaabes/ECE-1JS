7d688c8f4a107813909145837397f124bd5fb0d3
anjali verma <av3090075@gamil.com>
2026-03-11 09:23:43 +0530

    loops

---
+++ script1.js
@@ -0,0 +1,21 @@
+//let i;
+for(let i = 0; i < 5; i++){
+    document.write(i + "<br>");
+}
+let i = 0;
+for(; i < 5; i++){
+    document.write(i + "<br>");
+}
+document.write("Loop stopped");
+for(key in object){
+ //code block to be executed
+}
+const person = {
+    fname: "Anita",
+    lname: "Sharma",
+    age: 30,
+};
+for(let x in person){
+   console.log(x + ": " + person[x]);
+}
+
