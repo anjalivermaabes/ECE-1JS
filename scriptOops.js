68198c02831abfc50e15274bf694236b8658cf58
anjali verma <av3090075@gamil.com>
2026-03-18 09:53:19 +0530

    work

---
+++ scriptOops.js
@@ -0,0 +1,13 @@
+/*
+   const result=document.getElementById("id");
+    result.innerHTML = "Hello World";*/
+    const result1=document.getElementById("id1");
+    result[1].innerHTML = "Hello World";
+    const result2=document.getElementById("id2");
+    result[2].innerHTML = "Hello World";
+    
+    const result=document.getElementById("jK").getAttribute("class");
+
+document.getElementById("p1").innerHTML = result;
+
+   
