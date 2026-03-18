c07083492e7d4e93573418ec0ac9e1acceb90558
anjalivermaabes <anjv12345678@gmail.com>
2026-03-18 10:12:41 +0530

    work

---
+++ script1og.js
@@ -0,0 +1,15 @@
+var myinfo = new Array();
+var myinfo2 = Array();
+//ARRAY IS MUTABLE IT CAN BE EDDITED
+myinfo[0] = "John";
+myinfo[1] = "Smith";
+myinfo[2] = 30;
+myinfo[3] = "Engineer";
+
+myinfo2[0] = "Alice";
+myinfo2[1] = "Johnson";
+myinfo2[2] = 25;
+myinfo2[3] = "Designer";
+console.log(myinfo);
+console.log(myinfo2);
+
---
+++ scriptog.js
@@ -0,0 +1,75 @@
+/*console.log("Hello, World!");
+var num =10;
+console.log(num);
+console.log(typeof num);
+var num = "10";
+console.log(num);
+console.log(typeof num);
+var num = null;
+console.log(num);
+console.log(typeof num);
+var num = undefined;
+console.log(num);
+console.log(typeof num);
+var person = {
+    name: "John",
+    age: 30,
+    city: "New York"
+};
+console.log(person);
+console.log(person.name);
+console.log(person.age);
+console.log(person.city);
+const sym1 = Symbol(4)
+console.log(sym1);
+const sym2 = Symbol(4)
+console.log(sym2);
+if(sym1 === sym2){
+    console.log("true");
+}
+else{
+    console.log("false");
+}*/
+var a = 10;
+var b = 20;
+var linebreak = a + b;
+console.log(linebreak);
+document.write(linebreak);
+var a = 10;
+var b = 20;
+var linebreak = a - b;
+console.log(linebreak);
+document.write(linebreak);
+var a = 10;
+var b = 20;
+var linebreak = a = b;
+console.log(linebreak);
+document.write(linebreak);
+var a = 10;
+var b = 20;
+var linebreak = a * b;
+console.log(linebreak);
+document.write(linebreak);
+var a = 10;
+var b = 20;
+var linebreak = a % b;
+console.log(linebreak);
+document.write(linebreak);
+var a = 10;
+var b = 20;
+var linebreak = a / b;
+console.log(linebreak);
+document.write(linebreak);
+//coditional statements
+//if(condition){
+    //code block to be executed if condition is true}
+    //else if(condition){
+        //code block to be executed if condition is true}
+        //else{
+            //code block to be executed if condition is false}
+//let age = 15;
+//if(age > 18){
+    //console.log("You are an adult");}
+    //else{
+        //console.log("You are a minor");
+    //}
