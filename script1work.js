8d78065d24760f9dd3fb54b13a9cbb46387719ec
anjali verma <av3090075@gamil.com>
2026-03-18 09:58:44 +0530

    New work on js

---
+++ script1work.js
@@ -0,0 +1,95 @@
+//Loops in javascript 
+//if.... , if...else  .... , if....else if.... , switch case 
+//for loop, while loop, do...while loop, for...in loop, for...of loop
+//let i;
+//for(let i = 0; i < 5; i++){
+    //document.write(i + "<br>");
+//}
+//let i = 0;
+//for(; i < 5; i++){
+    //document.write(i + "<br>");
+//}
+//document.write("Loop stopped");
+//for(key in object){
+ //code block to be executed
+//}
+//const person = {
+  //  fname: "Anita",
+    //lname: "Sharma",
+    //age: 30,
+//};
+//for(let x in person){
+  // console.log(x + ": " + person[x]);
+//}
+//coditional statements
+//if(condition){
+    //code block to be executed if condition is true}
+    //else if(condition){
+        //code block to be executed if condition is true}
+        //else{
+            //code block to be executed if condition is false}
+//let age = 15;
+//if(age > 18){
+    //console.log("You are an adult");}
+    //else{
+        //console.log("You are a minor");
+    //}
+    //let grade = 0;
+//switch(grade)
+//{
+  //  case 90:
+    //    console.log("A");
+      //  break;
+    //case 80:
+      //  console.log("B");
+       // break;
+    //case 70:
+      //  console.log("C");
+        //break;
+    //case 60:
+      //  console.log("D");
+        //break;
+    //default:
+     
+    //console.log("F");
+//}
+//function
+function  myFunction(){
+  alert("Hello World");
+}
+function sayHello(name,age){
+    document.write(name + "is " + age + " years old.");
+}
+//Arrow function is an announonymous function which can be assigned to a variable
+
+//Normal Function 
+function add(a,b){
+    console.log(a + b);
+}
+add(5,10);
+//Arrow function
+const add1 = (a,b) => {
+    console.log(a + b);
+}
+add1(5,10);
+
+let person ={
+    name: "Anita",
+    age: 30,
+    isMarried: false,
+    hobbies: ["reading", "traveling", "cooking"],
+    address: {
+        street: "123 lane streert",
+        city: "Gurgaon",
+        state: "Haryana",
+        country: "India"
+}
+};
+console.log(person);
+console.log(person.name);
+console.log(person.age);
+console.log(person.isMarried);
+console.log(person.hobbies);
+console.log(person.address);
+/*let person ={
+
