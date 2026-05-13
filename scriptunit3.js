ff6005b4c8b6b99b3721d43c6816965710fe4bcb
anjalivermaabes <anjv12345678@gmail.com>
2026-05-13 09:49:17 +0530

    Worrk done

---
+++ scriptunit3.js
@@ -0,0 +1,28 @@
+/*component
+    1functional component
+    2class component
+    STATE MANAGEMENT - 
+    function Counter()
+    {
+    const[count,setCount] = useState(0);
+    return <button onClick={()=>setCount(count+1)}>{count}</button>
+   }
+    IF WE ARE USING JAVASCRIOPT IN HTML THEN WE HAVE TO USE {} BRACES
+
+    npm rundev (TO START SERVER AGAIN AND AGAIN)
+
+    JSX COMPONENTS :
+    jsx is not html,
+    transpiled to js by babel.
+    jsx must return single element (div or section) - parent element
+     
+    COMPONENTS ARE THE INDEPENDENT AND REUSABLE PIECES OF UI. THEY CAN BE FUNCTIONAL OR CLASS-BASED.
+
+     NAMING CONVENTIONS:
+      Comonents name start with a cpaital letter.
+        file name should be same as component name.
+        One component per file.
+        Component name should be same as file name.
+        If we have to add comment in jsx then we have to use {} and inside that we have to write comment like this {/* comment 
+        
+        */.
